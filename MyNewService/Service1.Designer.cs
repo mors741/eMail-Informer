de12fc15c709df3a3a56da7ed55251fe44a12e38
Kharitonov Evgeny <mors741@yandex.ru>
2014-10-07 12:17:31 +0400

    Initial commit

---
+++ MyNewService/Service1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MyNewService
+{
+    partial class MyNewService
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // MyNewService
+            // 
+            this.ServiceName = "MyNewService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
